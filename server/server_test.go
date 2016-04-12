4e366c496fe952bdab79f3ce8a31dc1d20f25ca1
Rafael George <george.rafael@gmail.com>
2016-04-12 14:22:45 -0400

    Add delete endpoint to server Add testcase for successful case Add testcase for unsuccessful case - Not created instance

--- server/server_test.go
+++ server/server_test.go
@@ -3,6 +3,7 @@ package server
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"net/http/httptest"
@@ -329,4 +330,36 @@ func TestDeployMissing(t *testing.T) {
 	err = json.Unmarshal(w.Body.Bytes(), &errorResponse)
 	assert.Nil(t, err)
 	assert.Contains(t, errorResponse["error"], "Not Found")
+}
+
+func TestDeleteWhenExistentInstance(t *testing.T) {
+	testInstance1 := &instance.Instance{
+		PlaybookID: "helloplaybook",
+		ID:         "TestGetStatusWithGoodPath",
+		Status:     instance.StatusDeployed}
+	is := services.NewInstanceService(store.New())
+	_, err := is.Create(testInstance1)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	req, w := testutils.DeleteRequest(
+		t,
+		fmt.Sprintf("/%s/%s", testInstance1.PlaybookID, testInstance1.ID),
+	)
+
+	req = auth(req)
+	makeRequest(req, w)
+
+	assert.Equal(t, http.StatusOK, w.Code, "Expected DELETE /instances to return 200")
+	assert.Contains(t, w.Body.String(), "Instance successfully deleted")
+}
+
+func TestDeleteWhenNonExistantInstance(t *testing.T) {
+	req, w := testutils.DeleteRequest(t, fmt.Sprintf("/%s/%s", "nonehere", "noid"))
+
+	req = auth(req)
+	makeRequest(req, w)
+
+	assert.Equal(t, http.StatusNotFound, w.Code, "Expected DELETE /instances to return 404 when missing instance")
 }
